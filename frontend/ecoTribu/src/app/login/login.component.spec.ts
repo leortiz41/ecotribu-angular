02ff53ddb05527fc445636fa952a27a061cb399f
leortiz41 <60624119+leortiz41@users.noreply.github.com>
2026-06-11 03:27:36 -0600

    Componente de Login

--- frontend/ecoTribu/src/app/login/login.component.spec.ts
+++ frontend/ecoTribu/src/app/login/login.component.spec.ts
@@ -1,7 +1,12 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LoginComponent } from './login.component';
+import { NgOptimizedImage } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
+describe('LoginComponent', () => {
+  let component: LoginComponent;
+  let fixture: ComponentFixture<LoginComponent>;
 describe('LoginComponent', () => {
   let component: LoginComponent;
   let fixture: ComponentFixture<LoginComponent>;
